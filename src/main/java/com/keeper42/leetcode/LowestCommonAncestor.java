8623a1533ec611cf7407ae32101b162760827ae5
keeper42 <szuljf@163.com>
2022-04-07 16:45:04 +0800

    lowestCommonAncestor

---
+++ src/main/java/com/keeper42/leetcode/LowestCommonAncestor.java
@@ -0,0 +1,40 @@
+package com.keeper42.leetcode;
+
+/**
+ * @author lijiafan
+ * @version 1.0
+ * @date 2022/4/7
+ * @type leetcode 236. 二叉树的最近公共祖先
+ */
+public class LowestCommonAncestor {
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // LCA 问题
+        if (root == null) {
+            return root;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        }
+        return null;
+    }
+
+}
