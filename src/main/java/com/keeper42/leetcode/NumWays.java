a2f9f00a5c8ac5cea3b26831f4f7cda8e299a55e
keeper42 <szuljf@163.com>
2022-04-09 07:53:45 +0800

    update

---
+++ src/main/java/com/keeper42/leetcode/NumWays.java
@@ -0,0 +1,22 @@
+package com.keeper42.leetcode;
+
+/**
+ * @author lijiafan
+ * @version 1.0
+ * @date 2022/4/9
+ */
+public class NumWays {
+
+    public int numWays(int n) {
+        if(n <= 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        //dp[2] = 2;
+        for(int i = 2; i < n+1; i++){
+            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+        }
+        return dp[n];
+    }
+
+}
