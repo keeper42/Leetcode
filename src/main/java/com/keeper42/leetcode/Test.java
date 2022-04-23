2d58b43654ede7347931245d01952282cbd53bcf
keeper42 <szuljf@163.com>
2022-04-23 23:56:19 +0800

    tests

---
+++ src/main/java/com/keeper42/leetcode/Test.java
@@ -0,0 +1,14 @@
+package com.keeper42.leetcode;
+
+/**
+ * @author lijiafan
+ * @version 1.0
+ * @date 2022/4/23
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("tests");
+    }
+
+}
